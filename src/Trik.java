819812a6d7e31c0ca6bd3d484e0eae4b245ed3bd
iti5706021411077 <5706021411077@fitm.kmutnb.ac.th>
2016-02-10 21:24:58 +0700

    Revert "ส่งงานค่ะ"
    This reverts commit 6bcd636cb7fd661c7b78be3b1678880244b76387.

--- src/Trik.java
+++ src/Trik.java
@@ -1,43 +1,10 @@
-import java.util.Scanner;
+
 public class Trik {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int r=1,c=0,l=0;
-		int tmp=0;
-		String text = null;
-		char Char;
-		Scanner scan =  new Scanner(System.in);
-		do{
-		System.out.print("Enter Value 50 character (A or B or C) : "); 
-		text=scan.next();
-		}while(text.length() > 50 || text.length() < 1 || text.length() == 0);
-		for(int i=0;i<text.length();i++)
-		{
-			Char=text.charAt(i);
-			if(Char=='A'|| Char=='a')
-			{
-				tmp=r;
-				r=c;
-				c=tmp;
-			}
-			if(Char=='B'|| Char=='b')
-			{
-				tmp=c;
-				c=l;
-				l=tmp;
-			}
-			if(Char=='C'|| Char=='c')
-			{
-				tmp=r;
-				r=l;
-				l=tmp;
-			}
-		}
-		if(r==1)System.out.println("glass 1");
-		if(c==1)System.out.println("glass 2");
-		if(l==1)System.out.println("glass 3");
+		System.out.println("Test");
 
 	}
 
